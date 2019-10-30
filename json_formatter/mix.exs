ed36bf908a9193e0948b7c1f18f152c144f1f07b
Tim Austin <tim@neenjaw.com>
2019-10-30 16:59:06 -0600

    bump version to 1.9.2

--- json_formatter/mix.exs
+++ json_formatter/mix.exs
@@ -5,7 +5,7 @@ defmodule JSONFormatter.MixProject do
     [
       app: :json_formatter,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.9.2",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
