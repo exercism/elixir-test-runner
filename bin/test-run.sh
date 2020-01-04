75aac1f98fc4afa95d81ed094d02431e403ee9fb
Tim Austin <tim@neenjaw.com>
2020-01-03 23:23:44 -0600

    future work

---
+++ bin/test-run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Synopsis: given an exercise UUID, build the docker image, then run it with the exercise
+# Assumption: you have the exercism binary in your path, you have privliges to download the solution
