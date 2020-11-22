a176ecb4fdd7d702d0d5f769f48f27d0263c8b42
Tim Austin <tim@neenjaw.com>
2020-11-22 03:06:00 -0600

    add assert parser

---
+++ exercism_formatter/lib/assert_parser.ex
@@ -0,0 +1,52 @@
+defmodule AssertParser do
+  def parse(assert_string) do
+    {:ok, comparator, command, expected} =
+      assert_string
+      |> Code.string_to_quoted!()
+      |> separate_assert()
+
+    expected_str = Macro.to_string(expected)
+    command_str = Macro.to_string(command)
+
+    {:ok, command_str, expected_to_phrase(comparator, expected_str)}
+  end
+
+  @doc """
+  separate_assert
+  Takes a function an quoted assert block and returns the command to be evaluated
+  to generate the actual value, and the expected value.
+  """
+  def separate_assert({:assert, _, [{comparator, _, [command, expected]}]})
+      when comparator in [:==, :===, :>=, :<=, :<, :>] do
+    {:ok, comparator, command, expected}
+  end
+
+  @doc """
+  Depending on the comparator, facilitate plain language translation
+  """
+  def expected_to_phrase(comparator, expected_str)
+
+  def expected_to_phrase(:===, expected_str) do
+    "to strict equal \"#{expected_str}\""
+  end
+
+  def expected_to_phrase(:==, expected_str) do
+    "to equal \"#{expected_str}\""
+  end
+
+  def expected_to_phrase(:>=, expected_str) do
+    "to be greater than or equal to \"#{expected_str}\""
+  end
+
+  def expected_to_phrase(:<=, expected_str) do
+    "to be less than or equal to \"#{expected_str}\""
+  end
+
+  def expected_to_phrase(:<, expected_str) do
+    "to be less than \"#{expected_str}\""
+  end
+
+  def expected_to_phrase(:>, expected_str) do
+    "to be greater than \"#{expected_str}\""
+  end
+end
---
+++ exercism_formatter/test/assert_parser_test.exs
@@ -0,0 +1,51 @@
+defmodule AssertParserTest do
+  use ExUnit.Case, async: false
+
+  describe "separate_assert" do
+    for op <- [:==, :===, :>=, :<=, :<, :>] do
+      @op op
+
+      test "#{@op}" do
+        assert_ast = "assert 1 #{@op} 2" |> Code.string_to_quoted!()
+        assert AssertParser.separate_assert(assert_ast) == {:ok, @op, 1, 2}
+      end
+    end
+  end
+
+  describe "expected_to_phrase" do
+    test "==" do
+      assert AssertParser.expected_to_phrase(:==, 1) == "to equal \"1\""
+    end
+
+    test "===" do
+      assert AssertParser.expected_to_phrase(:===, 1) == "to strict equal \"1\""
+    end
+
+    test ">=" do
+      assert AssertParser.expected_to_phrase(:>=, 1) == "to be greater than or equal to \"1\""
+    end
+
+    test "<=" do
+      assert AssertParser.expected_to_phrase(:<=, 1) == "to be less than or equal to \"1\""
+    end
+
+    test "<" do
+      assert AssertParser.expected_to_phrase(:<, 1) == "to be less than \"1\""
+    end
+
+    test ">" do
+      assert AssertParser.expected_to_phrase(:>, 1) == "to be greater than \"1\""
+    end
+  end
+
+  describe "parse" do
+    test "simple" do
+      assert AssertParser.parse("assert 1 == 1") == {:ok, "1", "to equal \"1\""}
+    end
+
+    test "expression" do
+      assert AssertParser.parse("assert 1 + 1 <= 2") ==
+               {:ok, "1 + 1", "to be less than or equal to \"2\""}
+    end
+  end
+end
