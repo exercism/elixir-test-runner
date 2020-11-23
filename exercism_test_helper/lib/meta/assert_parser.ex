5a9c38d7651ade647147c8ad16215831f5f91ef9
Tim Austin <tim@neenjaw.com>
2020-11-22 19:43:25 -0600

    add refute phrases

--- exercism_test_helper/lib/meta/assert_parser.ex
+++ exercism_test_helper/lib/meta/assert_parser.ex
@@ -1,6 +1,6 @@
 defmodule Meta.AssertParser do
   def parse(assert_string) do
-    {:ok, comparator, command, expected} =
+    {:ok, assertion, comparator, command, expected} =
       assert_string
       |> Code.string_to_quoted!()
       |> separate_assert()
@@ -8,7 +8,7 @@ defmodule Meta.AssertParser do
     command_str = Macro.to_string(command)
     expected_str = expected |> Macro.to_string() |> format_expected()
 
-    {:ok, command_str, expected_to_phrase(comparator, expected_str)}
+    {:ok, command_str, expected_to_phrase(assertion, comparator, expected_str)}
   end
 
   @doc """
@@ -16,41 +16,68 @@ defmodule Meta.AssertParser do
   Takes a function an quoted assert block and returns the command to be evaluated
   to generate the actual value, and the expected value.
   """
-  def separate_assert({:assert, _, [{comparator, _, [command, expected]}]})
-      when comparator in [:==, :===, :>=, :<=, :<, :>] do
-    {:ok, comparator, command, expected}
+  def separate_assert({assertion, _, [{comparator, _, [command, expected]}]})
+      when assertion in [:assert, :refute] and comparator in [:==, :===, :>=, :<=, :<, :>] do
+    {:ok, assertion, comparator, command, expected}
   end
 
   @doc """
-  Depending on the comparator, facilitate plain language translation
+  Depending on the assertion and comparator, facilitate plain language translation
   """
-  def expected_to_phrase(comparator, expected_str)
+  def expected_to_phrase(assertion, comparator, expected_str)
 
-  def expected_to_phrase(:===, expected) do
-    "to strict equal #{expected}"
+  # phrases for assert
+  def expected_to_phrase(:assert, :===, expected) do
+    "to be strict equal to #{expected}"
   end
 
-  def expected_to_phrase(:==, expected) do
-    "to equal #{expected}"
+  def expected_to_phrase(:assert, :==, expected) do
+    "to be equal to #{expected}"
   end
 
-  def expected_to_phrase(:>=, expected) do
+  def expected_to_phrase(:assert, :>=, expected) do
     "to be greater than or equal to #{expected}"
   end
 
-  def expected_to_phrase(:<=, expected) do
+  def expected_to_phrase(:assert, :<=, expected) do
     "to be less than or equal to #{expected}"
   end
 
-  def expected_to_phrase(:<, expected) do
+  def expected_to_phrase(:assert, :<, expected) do
     "to be less than #{expected}"
   end
 
-  def expected_to_phrase(:>, expected) do
+  def expected_to_phrase(:assert, :>, expected) do
     "to be greater than #{expected}"
   end
 
-  defp format_expected(str) when is_binary(str) do
+  # phrases for refute
+  def expected_to_phrase(:refute, :===, expected) do
+    "to not be strict equal to #{expected}"
+  end
+
+  def expected_to_phrase(:refute, :==, expected) do
+    "to not be equal to #{expected}"
+  end
+
+  def expected_to_phrase(:refute, :>=, expected) do
+    "to not be greater than or equal to #{expected}"
+  end
+
+  def expected_to_phrase(:refute, :<=, expected) do
+    "to not be less than or equal to #{expected}"
+  end
+
+  def expected_to_phrase(:refute, :<, expected) do
+    "to not be less than #{expected}"
+  end
+
+  def expected_to_phrase(:refute, :>, expected) do
+    "to not be greater than #{expected}"
+  end
+
+  @doc false
+  defp format_expected(str) do
     str
     # Commented out for now.
     # |> String.replace_leading(~S'"', "")
