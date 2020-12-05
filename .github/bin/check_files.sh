d949718ae837603049c5cf99a5fda44e846b25ae
Tim Austin <tim@neenjaw.com>
2020-12-05 14:21:41 -0600

    add smoke test

---
+++ .github/bin/check_files.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main {
+  expected_files=(metadata.json output error_log results.json output.json)
+
+  for file in ${expected_files[@]}; do
+    if [[ ! -f "./test/${file}" ]]; then
+      echo "🔥 expected ${file} to exist on successful run 🔥"
+      exit 1
+    fi
+  done
+}
+
+main "$@"; exit
