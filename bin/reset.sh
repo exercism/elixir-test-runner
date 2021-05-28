a25dbbbae8ba5630dfcd3235bac91f73a5862ad2
Tim Austin <tim@neenjaw.com>
2021-05-28 08:14:26 -0600

    rough with tools for testing new command

---
+++ bin/reset.sh
@@ -0,0 +1,7 @@
+# clear a run
+
+rm -rf /home/tim/Projects/exercism/elixir-test-runner/test/hello-world/_build
+rm /home/tim/Projects/exercism/elixir-test-runner/test/output
+rm /home/tim/Projects/exercism/elixir-test-runner/test/error_log
+rm /home/tim/Projects/exercism/elixir-test-runner/test/metadata.json
+rm /home/tim/Projects/exercism/elixir-test-runner/test/output.json
