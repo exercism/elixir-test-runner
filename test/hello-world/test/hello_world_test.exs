17067a9e4e59b09bd4b21f5f28b5224004be2562
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-08 15:43:00 +0200

    Use @tag instead of custom attribute for task id (#44)

--- test/hello-world/test/hello_world_test.exs
+++ test/hello-world/test/hello_world_test.exs
@@ -1,12 +1,12 @@
 defmodule(HelloWorldTest) do
   use(ExUnit.Case)
 
-  @task_id 1
+  @tag task_id: 1
   test("says 'Hello, World!'") do
     assert(HelloWorld.hello() == "Hello, World!")
   end
 
-  @task_id 2
+  @tag task_id: 2
   test("failing test") do
     assert [1, 2, 3] == [2, 3]
   end
